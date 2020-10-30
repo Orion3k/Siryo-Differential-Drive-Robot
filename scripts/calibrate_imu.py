0f7df4251c81b796f92478836f5ba73c5bb7458d
orion3k <10503183@polimi.it>
2020-10-30 23:23:59 +0000

    Fixed imu node with calibration (mean)

--- scripts/calibrate_imu.py
+++ scripts/calibrate_imu.py
@@ -3,86 +3,90 @@ import rospy
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
 
-n_samples = 10000
-acc_x = []
-acc_y = []
-acc_z = []
-gyr_x = []
-gyr_y = []
-gyr_z = []
-count = 0
+class Calibration:
 
-def callback(data):
-	count += 1
-	if count < n_samples:
-		acc_x.append(data.linear_acceleration.x)
-		acc_y.append(data.linear_acceleration.y)
-		acc_z.append(data.linear_acceleration.z)
-		gyr_x.append(data.angular_velocity.x)
-		gyr_y.append(data.angular_velocity.y)
-		gyr_z.append(data.angular_velocity.z)
-	else:
-		compute_mean_var()
+	def __init__(self):
+		self.n_samples = 1000
+		self.acc_x = []
+		self.acc_y = []
+		self.acc_z = []
+		self.gyr_x = []
+		self.gyr_y = []
+		self.gyr_z = []
+		self.count = 0
+		self.sub_imu_data()
 
-def compute_mean_var():
-	acc_mean = [0.,0.,0.]
-	for x in acc_x:
-		acc_mean[0] += x
-	for y in acc_y:
-		acc_mean[1] += y
-	for z in acc_z:
-		acc_mean[2] += z
+	def callback(self,data):
+		self.count += 1
+		#rospy.loginfo("Callback called")
+		if self.count < self.n_samples:
+			self.acc_x.append(data.linear_acceleration.x)
+			self.acc_y.append(data.linear_acceleration.y)
+			self.acc_z.append(data.linear_acceleration.z)
+			self.gyr_x.append(data.angular_velocity.x)
+			self.gyr_y.append(data.angular_velocity.y)
+			self.gyr_z.append(data.angular_velocity.z)
+		else:
+			self.compute_mean_var()
 
-	gyr_mean = [0.,0.,0.]
-	for x in gyr_x:
-		gyr_mean[0] += x
-	for y in gyr_y:
-		gyr_mean[1] += y
-	for z in gyr_z:
-		gyr_mean[2] += z
+	def compute_mean_var(self):
+		acc_mean = [0.,0.,0.]
+		for x in self.acc_x:
+			acc_mean[0] += x
+		for y in self.acc_y:
+			acc_mean[1] += y
+		for z in self.acc_z:
+			acc_mean[2] += z
 
-	acc_mean[0] = acc_mean[0]/n_samples
-	acc_mean[1] = acc_mean[1]/n_samples
-	acc_mean[2] = acc_mean[2]/n_samples
+		gyr_mean = [0.,0.,0.]
+		for x in self.gyr_x:
+			gyr_mean[0] += x
+		for y in self.gyr_y:
+			gyr_mean[1] += y
+		for z in self.gyr_z:
+			gyr_mean[2] += z
 
-	gyr_mean[0] = gyr_mean[0]/n_samples
-	gyr_mean[1] = gyr_mean[1]/n_samples
-	gyr_mean[2] = gyr_mean[2]/n_samples
+		acc_mean[0] = acc_mean[0]/self.n_samples
+		acc_mean[1] = acc_mean[1]/self.n_samples
+		acc_mean[2] = acc_mean[2]/self.n_samples
 
-	acc_var = [0.,0.,0.]
-	for x in acc_x:
-		acc_var[0] += (x - acc_mean[0])**2
-	for y in acc_y:
-		acc_var[1] += (y - acc_mean[1])**2
-	for z in acc_z:
-		acc_var[2] += (z - acc_mean[2])**2
+		gyr_mean[0] = gyr_mean[0]/self.n_samples
+		gyr_mean[1] = gyr_mean[1]/self.n_samples
+		gyr_mean[2] = gyr_mean[2]/self.n_samples
 
-	gyr_var = [0.,0.,0.]
-	for x in gyr_x:
-		gyr_var[0] += (x - gyr_mean[0])**2
-	for y in gyr_y:
-		gyr_var[1] += (y - gyr_mean[1])**2
-	for z in gyr_z:
-		gyr_var[2] += (z - gyr_mean[2])**2
+		acc_var = [0.,0.,0.]
+		for x in self.acc_x:
+			acc_var[0] += (x - acc_mean[0])**2
+		for y in self.acc_y:
+			acc_var[1] += (y - acc_mean[1])**2
+		for z in self.acc_z:
+			acc_var[2] += (z - acc_mean[2])**2
 
-	acc_var[0] = acc_var[0]/(n_samples-1)
-	acc_var[1] = acc_var[1]/(n_samples-1)
-	acc_var[2] = acc_var[2]/(n_samples-1)
-	gyr_var[0] = gyr_var[0]/(n_samples-1)
-	gyr_var[1] = gyr_var[1]/(n_samples-1)
-	gyr_var[2] = gyr_var[2]/(n_samples-1)
+		gyr_var = [0.,0.,0.]
+		for x in self.gyr_x:
+			gyr_var[0] += (x - gyr_mean[0])**2
+		for y in self.gyr_y:
+			gyr_var[1] += (y - gyr_mean[1])**2
+		for z in self.gyr_z:
+			gyr_var[2] += (z - gyr_mean[2])**2
 
-	print("acc mean: ",acc_mean, "acc var: ",acc_var)
-	print("gyro mean : ", gyr_var, "gyro var: ", gyr_var)
+		acc_var[0] = acc_var[0]/(self.n_samples-1)
+		acc_var[1] = acc_var[1]/(self.n_samples-1)
+		acc_var[2] = acc_var[2]/(self.n_samples-1)
+		gyr_var[0] = gyr_var[0]/(self.n_samples-1)
+		gyr_var[1] = gyr_var[1]/(self.n_samples-1)
+		gyr_var[2] = gyr_var[2]/(self.n_samples-1)
 
-def sub_imu_data():
-	rospy.init_node('calibrate_imu',anonymous=True)
-	count = 0
-	imu_sub = rospy.Subscriber('imu_odom',Imu,callback)
-	rospy.spin()
+		print("acc mean: ",acc_mean, "acc var: ",acc_var)
+		print("gyro mean : ", gyr_mean, "gyro var: ", gyr_var)
+
+	def sub_imu_data(self):
+		rospy.init_node('calibrate_imu',anonymous=True)
+		imu_sub = rospy.Subscriber('imu_odom',Imu,self.callback)
+		rospy.spin()
 
 if __name__ == '__main__':
 	try:
-		sub_imu_data()
+		calib = Calibration()
 	except rospy.ROSInterruptException:
 		pass
--- scripts/imu_mpu.py
+++ scripts/imu_mpu.py
@@ -9,20 +9,26 @@ sensor = mpu6050(0x68)
 def get_data():
 	#sensor.set_accel_range(accel_range=sensor.ACCEL_RANGE_2G)
 	#sensor.set_gyro_range(gyro_range=sensor.GYRO_RANGE_250DEG)
+
+	acc_mean = [0.7250001757202134, 0.12694057202148465, 9.722326217529298]
+	acc_var = [0.0026197816704015024, 0.002954857995298211, 0.0035961123983046226]
+	gyr_mean = [-3.224709923664119, 0.8887633587786263, -3.266458015267172]
+	gyr_var = [0.00582360882127977, 0.006331332020558544, 0.005843476432787353]
+
 	i = Imu()
 	i.header.stamp = rospy.Time.now()
 	i.header.frame_id = 'imu_base'
 	l_acel_dic = sensor.get_accel_data(g=False)
 	l_acel = Vector3()
-	l_acel.x = l_acel_dic['x']
-	l_acel.y = l_acel_dic['y']
-	l_acel.z = l_acel_dic['z']
+	l_acel.x = l_acel_dic['x'] - acc_mean[0]
+	l_acel.y = l_acel_dic['y'] - acc_mean[1]
+	l_acel.z = l_acel_dic['z'] - acc_mean[2] + 9.81
 	i.linear_acceleration = l_acel
 	a_vel_dic = sensor.get_gyro_data()
 	a_vel = Vector3()
-	a_vel.x = a_vel_dic['x']
-	a_vel.y = a_vel_dic['y']
-	a_vel.z = a_vel_dic['z']
+	a_vel.x = a_vel_dic['x'] - gyr_mean[0]
+	a_vel.y = a_vel_dic['y'] - gyr_mean[1]
+	a_vel.z = a_vel_dic['z'] - gyr_mean[2]
 	i.angular_velocity = a_vel
 	return i
 
